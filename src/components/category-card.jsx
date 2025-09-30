8ba2732229e55a661df1751ca37f78ad7f7338d5
ALADETAN-IFE <famousfotune123@gmail.com>
2025-09-30 11:19:28 +0100

    Did the heropage, used one of the image link for the products

--- src/components/category-card.jsx
+++ src/components/category-card.jsx
@@ -4,7 +4,7 @@ import { FaCircleArrowRight } from "react-icons/fa6";
 const CategoryCard = () => {
   return (
     <div className="categoryCard">
-       <div className="categoryCard_img"></div>
+       <div className="categoryCard_img" style={{backgroundImage: `url(https://thedistillery.ng/wp-content/uploads/elementor/thumbs/Bombay-Sapphire-Drink-1-py27hbtogtini5rl9vknhiawitvsuilvynngl7afbi.jpg)`}}></div>
       <p className="categoryCard_text">Martell VS</p>
       <button className="categoryCard_btn button">
         Shop here
